d0cc867a557eaaec453289320a612ed4b768c666
vblacksquare <vblacksquare@vblacksquares-MacBook-Air.local>
2024-01-30 22:36:47 +0200

    NEW CLI FOR DEBUGGING version: 0.5-TURBO-CLI

---
+++ cli.py
@@ -0,0 +1,29 @@
+import json
+
+import requests
+
+
+def main():
+    url = "http://127.0.0.1:5000/search"
+
+    while True:
+        query = input("Search for movies: ")
+
+        data = {
+            "query": query,
+            "source": "imdb",
+            "filter": {
+                "type": "any",
+                "amount": 10
+            }
+        }
+
+        response = requests.get(url, json=data)
+        with open(f"./search_results/{query}.json", "w") as f:
+            json.dump(response.json(), f)
+
+        print(f"Writed search results to {query}.json\n")
+
+
+if __name__ == '__main__':
+    main()
